3e4c0d192d581fc202349a47140897d988ba56ee
FerreolS <ferreol.soulez@univ-lyon1.fr>
2025-03-31 20:54:43 +0200

    === instead of == for NaN

--- test/UnivariateStatistic_test.jl
+++ test/UnivariateStatistic_test.jl
@@ -2,8 +2,6 @@
     # Test for the UnivariateStatistic struct
     A = UnivariateStatistic(2, 0.5)
     B = UnivariateStatistic(1, Float32)
-    @test zero(A) == UnivariateStatistic(2, 0.0)
-    @test zero(B) == UnivariateStatistic(1, 0.0f0)
 
     @test A.rawmoments == [0.5, 0.0]
     @test A.weights == 1
@@ -11,17 +9,17 @@
     @test B.weights == 0
 
     @test mean(A) == 0.5
-    @test var(A) == NaN
-    @test skewness(A) == NaN
-    @test kurtosis(A) == NaN
+    @test var(A) === NaN
+    @test skewness(A) === NaN
+    @test kurtosis(A) === NaN
     @test nobs(A) == 1
 
-    C = UnivariateStatistic(2, Float64)
+    C = zero(A)
     push!(C, ones(10))
     @test mean(C) == 1.0
     @test var(C) == 0.0
 
-    D = UnivariateStatistic(2, Float64)
+    D = zero(typeof(A))
     push!(D, zeros(10))
 
     merge!(C, D)
