f842923722019106a744af93b4cbd29ee881214f
FerreolS <ferreol.soulez@univ-lyon1.fr>
2025-04-07 17:23:48 +0200

    using OnlineStatsBase

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,8 @@ using OnlineSampleStatistics
 using Test
 
 @testset "OnlineSampleStatistics.jl" begin
+    using OnlineStatsBase
+
     include("UnivariateStatistic_test.jl")
     include("IndependantStatistics_test.jl")
     # Write your tests here.
