e2b84c6439f780dce8886303189039340e35bf1b
buthanoid <portail@buthanoid.eu>
2026-03-18 11:03:38 +0100

    add public declaration

--- src/OnlineSampleStatistics.jl
+++ src/OnlineSampleStatistics.jl
@@ -13,6 +13,10 @@ export UnivariateStatistic,
     fit!,
     merge!
 
+VERSION >= v"1.11.0-DEV.469" && eval(Meta.parse(string(
+    "public STAT_HDU_KWD, STAT_GROUP_ID_KWD, STAT_MOMENT_INDEX_KWD, STAT_NB_MOMENTS_KWD, ",
+    "STAT_WEIGHTS_KWD, isa_stat_hdu, find_stat_group_ids, find_stat_hdus")))
+
 import OnlineStatsBase
 import Statistics
 import StatsBase
